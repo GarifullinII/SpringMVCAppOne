a687278813af5afb8890e11648532817d45b5c0a
GarifullinII <ildaridrisovich@gmail.com>
2026-02-06 23:08:22 +0400

    add RequestParam

--- src/main/java/ru/garifullin/mvc/controllers/FirstController.java
+++ src/main/java/ru/garifullin/mvc/controllers/FirstController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
@@ -20,9 +21,9 @@ public class FirstController {
 //    }
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname
+                            ) {
 
         System.out.println("Hello, " + name + " " + surname);
 
