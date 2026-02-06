c7853ebdcd31d3d348fcc6fddf199efb064c4be0
GarifullinII <ildaridrisovich@gmail.com>
2026-02-06 18:12:38 +0400

    add annotation controller, hello.html, goodbye.html, exit.html

---
+++ src/main/java/ru/garifullin/mvc/config/WebAppInitializer.java
@@ -0,0 +1,26 @@
+package ru.garifullin.mvc.config;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) {
+
+        AnnotationConfigWebApplicationContext context =
+                new AnnotationConfigWebApplicationContext();
+        context.register(SpringConfig.class);
+
+        DispatcherServlet dispatcherServlet =
+                new DispatcherServlet(context);
+
+        ServletRegistration.Dynamic dispatcher =
+                servletContext.addServlet("dispatcherServlet", dispatcherServlet);
+
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
