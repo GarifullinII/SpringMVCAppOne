0d55741d87c66c31da94b02b9ad857b09ee10627
GarifullinII <ildaridrisovich@gmail.com>
2026-02-06 22:58:09 +0400

    add HttpServletRequest

--- src/main/java/ru/garifullin/mvc/controllers/FirstController.java
+++ src/main/java/ru/garifullin/mvc/controllers/FirstController.java
@@ -1,5 +1,6 @@
 package ru.garifullin.mvc.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/first")
 public class FirstController {
 
+//    @GetMapping("/hello")
+//    public String helloPage(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//
+//        System.out.println("Hello, " + name + " " + surname);
+//
+//        return "first/hello";
+//    }
+
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
